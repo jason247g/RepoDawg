c52543ac9bac6d4aeff821eec87be3a7f9b5539f
jason247g <74936035+jason247g@users.noreply.github.com>
2025-10-22 12:44:38 -0400

    Sync DawgWizard folder under zips; rebuild 4.20 zip with correct prefix; refresh md5

---
+++ zips/plugin.program.dawgwizard/uservar.py
@@ -0,0 +1,14 @@
+'''#####-----Build File-----#####'''
+buildfile = 'https://raw.githubusercontent.com/jason247g/Dawg/main/wizard_builds.json'
+
+'''#####-----Videos File-----#####'''
+videos_url = 'https://raw.githubusercontent.com/jason247g/Dawg/main/videos.txt'
+
+'''#####-----Notification File-----#####'''
+notify_url  = 'https://raw.githubusercontent.com/jason247g/Dawg/main/notify.txt'
+
+'''#####-----Changelog Directory-----#####'''
+changelog_dir  = 'https://raw.githubusercontent.com/jason247g/Dawg/main/'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
