429cd313273b377a6c395112eb36d88845046db8
jason247g <74936035+jason247g@users.noreply.github.com>
2026-05-10 16:02:55 -0400

    Add files via upload

--- _stage_dw/plugin.program.dawgwizard/resources/lib/modules/plugin.py
+++ _stage_dw/plugin.program.dawgwizard/resources/lib/modules/plugin.py
@@ -159,4 +159,17 @@ def router(paramstring):
         message = notify.get_changelog()
         notify.notification(message)
         
+
+    elif mode == 125:
+        from .seren_tools import enable_and_auth
+        provider = p.get_params().get('provider')  # Params already parsed at top
+        enable_and_auth(provider)
+        
+    elif mode == 126:
+        from .umbrella_tools import enable_and_auth
+        provider = p.get_params().get('provider')  # Params already parsed at top
+        enable_and_auth(provider)
+
+
+        
     xbmcplugin.endOfDirectory(HANDLE)
